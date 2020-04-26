26eb8e8e2d54447bb9c659e0e2116f47c39d9fd4
Svytoslav <sv_borisov03@rambler.ru>
2020-04-26 17:56:53 +0400

    #31.4

--- static/scripts/scriptForBase.js
+++ static/scripts/scriptForBase.js
@@ -1,6 +1,6 @@
 window.onresize = function () {
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    var x = $( '#content' ).height() + 402 - Number($( '#content' ).css( "margin-bottom" ).substring(0, $( '#content' ).css( "margin-bottom" ).length - 2)) - Number($( '#content' ).css( "margin-top" ).substring(0, $( '#content' ).css( "margin-top" ).length - 2));
+    var x = $( '#content' ).height()+ 400 - Number($( '#content' ).css( "margin-bottom" ).substring(0, $( '#content' ).css( "margin-bottom" ).length - 2)) - Number($( '#content' ).css( "margin-top" ).substring(0, $( '#content' ).css( "margin-top" ).length - 2));
     if ( $( window ).height() > x + 130) {
         document.getElementById('footer').style.marginTop = String( Number($( document ).height() - x - 130)) + 'px';
     }
@@ -38,7 +38,7 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
                          <p id="footer_p">Онлайн Викторина © 2020 NothingNowhereNowhen.com</p>
                      </div>`
     document.getElementById('main').after(div);
-    var x = $( '#content' ).height() + 402 - Number($( '#content' ).css( "margin-bottom" ).substring(0, $( '#content' ).css( "margin-bottom" ).length - 2)) - Number($( '#content' ).css( "margin-top" ).substring(0, $( '#content' ).css( "margin-top" ).length - 2));
+    var x = $( '#content' ).height() + 400 - Number($( '#content' ).css( "margin-bottom" ).substring(0, $( '#content' ).css( "margin-bottom" ).length - 2)) - Number($( '#content' ).css( "margin-top" ).substring(0, $( '#content' ).css( "margin-top" ).length - 2));
     if ( $( window ).height() > x + 130) {
         document.getElementById('footer').style.marginTop = String( Number($( document ).height() - x - 130)) + 'px';
     }
